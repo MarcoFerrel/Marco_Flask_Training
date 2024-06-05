514a22350582391cd146ca19a5bd3e8c0613a2ee
MarcoFerrel <108032978+MarcoFerrel@users.noreply.github.com>
2024-06-05 13:55:25 -0600

    Job Posting Skeleton
    Job Postings json

--- views.py
+++ views.py
@@ -1,7 +1,46 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 
 views = Blueprint(__name__, "views")
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': "El Paso, Texas",
+        'salary': "$80,000"
+    },
+     {
+        'id': 2,
+        'title': 'Software Engineer',
+        'location': "Austin, Texas",
+        'salary': "$120,000"
+    },
+     {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': "Austin, Texas",
+        'salary': "$180,000"
+    }
+]
+
+
 @views.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", jobs=JOBS)
+
+@views.route("/profile")
+def profile():  
+    return render_template("profile.html")
+
+@views.route("/json")
+def get_json():
+     return  jsonify({'name': 'tim', 'coolness': 10})
+
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+@views.route("/go-to-home")
+def  go_to_home():
+    return redirect(url_for("views.home"))
